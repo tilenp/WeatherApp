fcd52635636d20565a221a436844020dc20aee83
tilenp <tilen.prepeluh@gmail.com>
2026-02-01 21:33:58 +0100

    domain module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,9 @@ android {
 }
 
 dependencies {
+    /** project modules **/
+    implementation(project(":domain"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "WeatherApp"
 include(":app")
- 
+include(":domain")
