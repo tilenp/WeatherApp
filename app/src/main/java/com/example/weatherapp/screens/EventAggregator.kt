9f68d7d0416ba7ad0707690a6e1d7e383d46790a
tilenp <tilen.prepeluh@gmail.com>
2026-02-02 23:09:19 +0000

    event aggregator

---
+++ app/src/main/java/com/example/weatherapp/screens/EventAggregator.kt
@@ -0,0 +1,16 @@
+package com.example.weatherapp.screens
+
+import com.example.domain.model.GeocodingItem
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+
+class EventAggregator {
+
+    private val _selectedGeocodingItem = MutableSharedFlow<GeocodingItem>(replay = 1)
+    val selectedGeocodingItem: SharedFlow<GeocodingItem> = _selectedGeocodingItem
+
+    suspend fun setSelectedGeocodingItem(geocodingItem: GeocodingItem) {
+        _selectedGeocodingItem.emit(geocodingItem)
+    }
+}
+
