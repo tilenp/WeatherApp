21fa0c4c6129ac3c9dbf92a4d88fb57bbc998f26
tilenp <tilen.prepeluh@gmail.com>
2026-02-03 08:20:12 +0000

    resources

---
+++ app/src/main/java/com/example/weatherapp/ui/theme/Dimens.kt
@@ -0,0 +1,20 @@
+package com.example.weatherapp.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val spacing0 = 0.dp
+    val spacing2 = 2.dp
+    val spacing4 = 4.dp
+    val spacing6 = 6.dp
+    val spacing8 = 8.dp
+    val spacing12 = 12.dp
+    val spacing16 = 16.dp
+    val spacing24 = 24.dp
+}
+
+object Sizes {
+    val microIcon = 16.dp
+    val smallIcon = 24.dp
+    val mediumIcon = 48.dp
+}
